89633b68ad384593e0ea10494b39eb050fcd0d7b
Jason Brown <jasonbrown@fastmail.fm>
2020-05-02 15:49:19 +0200

    created new deck type

--- cards/main.go
+++ cards/main.go
@@ -1,17 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := []string{"Six of Diamonds", newCard()}
+	cards := newDeck()
 
-	cards = append(cards, "King of Spades")
-
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-	return "Ace of Spades"
+	cards.print()
 }
