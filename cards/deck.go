b9e8aa1f02b55157669ac92d542390d1015fb190
Jason Brown <jasonbrown@fastmail.fm>
2020-05-03 15:07:24 +0200

    completed the card deck section of the course.  final code

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 // create a type for deck that is a slice of strings
 type deck []string
@@ -20,8 +27,49 @@ func newDeck() deck {
 	return cards
 }
 
+func (d deck) deal(handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		// Option 1 - log the error and return a deck using newDeck()
+		// Option 2 - log the error and exit the program
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	// create a random number source using the current time as the seed
+	source := rand.NewSource(time.Now().UnixNano())
+	// create a new random number generator based on the new source
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		// swap the cards at the i-th and newPosition-th
+		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
---
+++ cards/deck_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestNewDeck(t *testing.T) {
+	cards := newDeck()
+
+	if len(cards) != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", len(cards))
+	}
+}
+
+func TestSaveDeckToFileAndLoadDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
